2223162b8d2c8953051a7dc3c8e3d72ad70ab605
Jesse Gietzen <gietzenjesse@gmail.com>
2020-12-10 12:28:30 -0700

    assign variable with delimited input

--- resistor-color-duo/resistor_color_duo.rb
+++ resistor-color-duo/resistor_color_duo.rb
@@ -1,8 +1,5 @@
 class ResistorColorDuo
-  COLOR_MAP = [
-    'black', 'brown', 'red', 'orange', 'yellow',
-    'green', 'blue', 'violet', 'grey', 'white'
-  ]
+  COLOR_MAP = %w[black brown red orange yellow green blue violet grey white].freeze
 
   def self.value(colors)
     colors.first(2).map { |color| COLOR_MAP.index(color) }.join.to_i

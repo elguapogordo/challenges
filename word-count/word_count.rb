ed2c83dc745c97d348490ab51f633fde94676f24
Jesse Gietzen <gietzenjesse@gmail.com>
2020-12-16 17:32:58 -0700

    add methods

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -1,2 +1,13 @@
 class Phrase
+  def initialize(phrase)
+    @phrase = phrase.split('\n').join
+    @counts = Hash.new { |hash, key| hash[key] = 0 }
+  end
+
+  def word_count
+    @phrase.scan(/(?!'.*')\b[\w']+\b/).each do |word|
+      @counts[word.downcase] += 1
+    end
+    @counts
+  end
 end

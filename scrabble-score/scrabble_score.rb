d8df1813eca56348df4c9dab4c7f727c59de9400
Jesse Gietzen <gietzenjesse@gmail.com>
2020-12-20 23:54:42 -0700

    add class, methods

---
+++ scrabble-score/scrabble_score.rb
@@ -0,0 +1,23 @@
+class Scrabble
+  SCORE = {
+    a: 1, b: 3, c: 3, d: 2, e: 1, f: 4, g: 2, h: 4, i: 1,
+    j: 8, k: 5, l: 1, m: 3, n: 1, o: 1, p: 3, q: 10,
+    r: 1, s: 1, t: 1, u: 1, v: 4, w: 4, x: 8, y: 4, z: 10
+  }.freeze
+
+  def initialize(input)
+    @word = if input
+              input.downcase.scan(/(?!'.*')\b[\w']+\b/).join
+            else
+              ''
+            end
+  end
+
+  def score
+    @word.chars.map { |char| SCORE[char.to_sym] }.sum
+  end
+
+  def self.score(input)
+    Scrabble.new(input).score
+  end
+end

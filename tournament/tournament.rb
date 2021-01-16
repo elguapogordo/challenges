e4b2c3c20babd5c38575a452a43c855f8733313f
Jesse Gietzen <gietzenjesse@gmail.com>
2021-01-15 23:32:07 -0700

    pass one test

--- tournament/tournament.rb
+++ tournament/tournament.rb
@@ -1,4 +1,13 @@
 class Tournament
-  def tally(input)
+  def self.tally(input)
+    banner + standings(input).to_s
+  end
+
+  def self.banner
+    "Team                           | MP |  W |  D |  L |  P\n"
+  end
+
+  def self.standings(input)
+    # require 'pry'; binding.pry
   end
 end
